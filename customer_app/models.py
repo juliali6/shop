141d1d7b7dd04e64c6793a265dfa6604907b6dfb
Юлия Кирик <101558278+juliali6@users.noreply.github.com>
2022-09-05 15:37:59 +0300

    added orders

--- customer_app/models.py
+++ customer_app/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
+from django.core.validators import RegexValidator
 from django.db import models
-
+from django.utils import timezone
 
 User = get_user_model()
 
@@ -10,6 +11,63 @@ class Customer(models.Model):
     user = models.ForeignKey(User,verbose_name='User', on_delete=models.CASCADE)
     phone = models.CharField(max_length=20, verbose_name='Phone number', null=True, blank=True)
     address = models.CharField(max_length=255, verbose_name='Address', null=True, blank=True)
+    orders = models.ManyToManyField('Order', verbose_name='Buyer orders', related_name='related_customer')
 
     def __str__(self):
         return 'Customer: {} {}'.format(self.user.first_name, self.user.last_name)
+
+
+class Order(models.Model):
+
+    STATUS_NEW = 'new'
+    STATUS_IN_PROGRESS = 'in_progress'
+    STATUS_READY = 'is_ready'
+    STATUS_COMPLETED = 'completed'
+
+    BUYING_TYPE_SELF = 'self'  # самовывоз
+    BUYING_TYPE_DELIVERY = 'delivery'  # доставка
+
+    STATUS_CHOICES = (
+        (STATUS_NEW, 'New order'),
+        (STATUS_IN_PROGRESS, 'Order in progress'),
+        (STATUS_READY, 'Order is ready'),
+        (STATUS_COMPLETED, 'Order completed'),
+    )
+
+    BUYING_TYPE_CHOICES = (
+        (BUYING_TYPE_SELF, 'Pickup'),
+        (BUYING_TYPE_DELIVERY, 'Delivery'),
+    )
+
+    customer = models.ForeignKey(Customer, verbose_name='Buyer', related_name='related_orders', on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255, verbose_name='First name')
+    last_name = models.CharField(max_length=255, verbose_name='Last name')
+    phone = models.CharField(
+        validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$')],
+        max_length=17,
+        verbose_name='Mobile phone',
+        blank=True,
+        null=True,
+    )
+    address = models.CharField(max_length=1024, verbose_name='Address', null=True, blank=True)
+    status = models.CharField(
+        max_length=100,
+        verbose_name='Status order',
+        choices=STATUS_CHOICES,
+        default=STATUS_NEW
+    )
+    buying_type = models.CharField(
+        max_length=100,
+        verbose_name='Type order',
+        choices=BUYING_TYPE_CHOICES,
+        default=BUYING_TYPE_SELF,
+    )
+    comment = models.TextField(verbose_name='Comment', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now=True, verbose_name='Order created date')
+    order_date = models.DateField(verbose_name='Date of receipt of the order', default=timezone.now)
+
+    def __str__(self):
+        return str(self.id)
+
+
+
