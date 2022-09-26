1af4fee2a358bfa219926a6f994d68a19ffc7b26
Юлия Кирик <101558278+juliali6@users.noreply.github.com>
2022-09-26 16:06:22 +0300

    added

--- shop_django/settings.py
+++ shop_django/settings.py
@@ -146,17 +146,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'staticfiles')
-STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "static/"
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-# STATIC_DIRS = (
-#     os.path.join(BASE_DIR, 'static_dev')
-# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
