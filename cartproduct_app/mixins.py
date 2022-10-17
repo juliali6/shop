2bfb350c10251248bbc23f58f0dc4acc5e5548a0
Name <yulia.everli@gmail.com>
2022-10-17 18:23:03 +0300

    try fix

--- cartproduct_app/mixins.py
+++ cartproduct_app/mixins.py
@@ -1,4 +1,4 @@
-from django.views.generic import View
+from django.views import View
 
 from cartproduct_app.models import Cart
 from customer_app.models import Customer
@@ -11,20 +11,22 @@ class CartMixin(View):
         Затем ищем корзину, которая относится к пользователю.
          Если она найдена мы ее возвращаем, если нет, то создаем новую корзину и возвращаем ее. """
 
+        cart = None
         if request.user.is_authenticated:
             customer = Customer.objects.filter(user=request.user).first()
             if not customer:
                 customer = Customer.objects.create(
-                    user=request.user,
+                    user=request.user
                 )
             cart = Cart.objects.filter(owner=customer, in_order=False).first()
             if not cart:
                 cart = Cart.objects.create(owner=customer)
-
-        else:
-            cart = Cart.objects.filter(for_anonymous_user=True).first()
-            if not cart:
-                cart = Cart.objects.create(for_anonymous_user=True)
-                return cart
         self.cart = cart
         return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cart'] = self.cart
+        return context
+
+
